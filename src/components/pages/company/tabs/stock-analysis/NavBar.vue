634e9a1ff7c6e98e7cbf116867fddf8527ffe7e9
pkhodakovsky <pavel.khodakovsky1996@gmail.com>
2022-07-08 16:35:41 +0300

    Added missing components to Company -> Stock Analysis -> Operations

--- src/components/pages/company/tabs/stock-analysis/NavBar.vue
+++ src/components/pages/company/tabs/stock-analysis/NavBar.vue
@@ -3,7 +3,12 @@
     <NavBarItems @changeNavBarItem="changeNavBarItem" :nav-items="navItems" />
 
     <div class="flex items-center">
-      <TimeSelect class="mr-6" :items="period" :selected="'Last 5 Years'" />
+      <TimeSelect class="mr-5" :items="period" :selected="'Last 5 Years'" />
+
+      <div v-if="navItems.active === 'operations'" class="mr-5 flex items-center">
+        <span class="mr-3.5 text-grey text-sm leading-5 font-medium">Show Competition</span>
+        <SwitchCheckbox :active="true" />
+      </div>
 
       <ThreeCircleDropBtn />
     </div>
@@ -11,36 +16,38 @@
 </template>
 
 <script>
-  import NavBarItems from "@/components/pages/company/tabs/stock-analysis/summary/NavBarItems";
-  import TimeSelect from "@/components/universal/TimeSelect";
-  import ThreeCircleDropBtn from "@/components/universal/ThreeCircleDropBtn";
+import NavBarItems from "@/components/pages/company/tabs/stock-analysis/summary/NavBarItems";
+import TimeSelect from "@/components/universal/TimeSelect";
+import ThreeCircleDropBtn from "@/components/universal/ThreeCircleDropBtn";
+import SwitchCheckbox from "@/components/universal/SwitchCheckBox";
 
-  export default {
-    components: {
-      ThreeCircleDropBtn,
-      TimeSelect,
-      NavBarItems
-    },
-    props: {
-      navItems: {
-        type: Object,
-        default: () => {}
-      }
-    },
-    data: () => ({
-      period: [
-        'Last 1 year',
-        'last 2 years',
-        'last 3 years',
-        'last 4 years',
-        'last 5 years',
-        'last 6 years',
-      ],
-    }),
-    methods: {
-      changeNavBarItem(id) {
-        this.$emit('changeNavBarItem', id)
-      }
+export default {
+  components: {
+    SwitchCheckbox,
+    ThreeCircleDropBtn,
+    TimeSelect,
+    NavBarItems
+  },
+  props: {
+    navItems: {
+      type: Object,
+      default: () => {}
     }
+  },
+  data: () => ({
+    period: [
+      'Last 1 year',
+      'last 2 years',
+      'last 3 years',
+      'last 4 years',
+      'last 5 years',
+      'last 6 years',
+    ],
+  }),
+  methods: {
+    changeNavBarItem(id) {
+      this.$emit('changeNavBarItem', id)
+    },
   }
+}
 </script>
--- src/components/pages/company/tabs/stock-analysis/operations/Head.vue
+++ src/components/pages/company/tabs/stock-analysis/operations/Head.vue
@@ -9,23 +9,31 @@
         </div>
         <div class="h-5 w-5 cursor-pointer ml-1.5">
           <img
-            class="h-full w-full object-contain"
-            :src="require('@/assets/img/pages/profile/info-icon.svg')"
-            alt="Info icon"
+              class="h-full w-full object-contain"
+              :src="require('@/assets/img/pages/profile/info-icon.svg')"
+              alt="Info icon"
           />
         </div>
       </div>
 
-      <ThreeCircleDropBtn />
+      <div class="flex items-center">
+        <GridBtn class="mr-2" />
+        <ThreeCircleDropBtn />
+      </div>
     </div>
 
     <div class="pt-[18px] flex justify-between">
-      <div class="max-w-[524px] text-sm leading-4 text-secondary-text">
+      <div class="text-sm leading-4 text-secondary-text">
         What the market is currently paying based on a company's financial prospects, and what the market has been willing to pay in the past.
       </div>
 
-      <div>
-
+      <div class="max-w-[352px] py-3 px-2 grid grid-cols-2 gap-y-1 gap-x-[34px]">
+        <div v-for="item in list" :key="item.id" class="flex items-center">
+          <div class="h-4 w-4 mr-1">
+            <img class="h-full w-full" :src="item.icon" :alt="`${item.title} icon`">
+          </div>
+          <div class="text-sm leading-4 text-grey underline">{{ item.title }}</div>
+        </div>
       </div>
     </div>
   </section>
@@ -33,7 +41,21 @@
 
 <script>
 import ThreeCircleDropBtn from "@/components/universal/ThreeCircleDropBtn";
+import GridBtn from "@/components/universal/GridBtn";
 export default {
-  components: {ThreeCircleDropBtn}
+  components: {
+    GridBtn,
+    ThreeCircleDropBtn
+  },
+  data: () => ({
+    list: [
+      { id: '1', title: 'Sales & Earnings', icon: require('@/assets/img/pages/company/operations/bull-green.svg') },
+      { id: '2', title: 'Margins', icon: require('@/assets/img/pages/company/operations/bear-red.svg') },
+      { id: '3', title: 'Costs', icon: require('@/assets/img/pages/company/operations/bear-red.svg') },
+      { id: '4', title: 'Earnigns per share', icon: require('@/assets/img/pages/company/operations/bull-green.svg') },
+      { id: '5', title: 'Profits', icon: require('@/assets/img/pages/company/operations/bull-green.svg') },
+      { id: '6', title: 'Outlook', icon: require('@/assets/img/pages/company/operations/bear-red.svg') },
+    ]
+  })
 }
 </script>
